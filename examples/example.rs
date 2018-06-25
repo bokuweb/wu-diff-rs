dca9808c207995e031b182d513bd40b8d202e0f0
bokuweb <bokuweb@users.noreply.github.com>
2018-06-25 15:28:05 +0900

    Update example.rs

--- examples/example.rs
+++ examples/example.rs
@@ -3,12 +3,9 @@ extern crate wu_diff;
 use self::wu_diff::*;
 
 fn main() {
-    // let old = vec!["foo", "bar", "baz"];
-    // let new = vec!["foo", "baz", "hoge"];
+    let old = vec!["foo", "bar", "baz"];
+    let new = vec!["foo", "baz", "hoge"];
 
-    wu_diff::diff(&[0u8; 20000], &[1u8; 20000]);
-
-    /*
     for diff in wu_diff::diff(&old, &new) {
         match diff {
             DiffResult::Added(a) => println!("+{} new index = {}", a.data, a.new_index.unwrap()),
@@ -21,5 +18,4 @@ fn main() {
             DiffResult::Removed(r) => println!("-{} old index = {}", r.data, r.old_index.unwrap()),
         }
     }
-    */
 }
